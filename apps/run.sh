66f3f8ea5e1a0b36b4bf49fc6e0fe52ede9aa8e0
yyppyy <yuyanpeng@pku.edu.cn>
2022-03-07 07:35:56 -0800

    fix

--- apps/run.sh
+++ apps/run.sh
@@ -1,1 +1,1 @@
-./PageRank -nWorkers $1 -maxIters $2 -numberOfUpdateBatches 1 -nEdges 0 -streamPath ../inputs/sample_edge_operations.empty.txt /media/data_ssds/yanpeng/com-youtube/com-youtube.adj
+./PageRank -nWorkers $1 -maxIters $2 -numberOfUpdateBatches 1 -nEdges 0 -streamPath ../inputs/sample_edge_operations.empty.txt /test_program_log/graphbolt/com-youtube.adj
